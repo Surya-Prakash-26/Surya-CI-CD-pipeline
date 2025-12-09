7630ec22d09a5deb347c30010695eecf892f06d7
Surya-Prakash-26 <suryalogu616@gmail.com>
2025-12-09 23:53:15 +0530

    Updated pom.xml, App.java and AppTest.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,12 @@
 package com.example;
 
 public class App {
+
+    public static String mainMessage() {
+        return "Hello from Surya's CI/CD Pipeline!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello from Surya's CI/CD Pipeline!");
+        System.out.println(mainMessage());
     }
 }
